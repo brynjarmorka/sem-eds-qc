0c0e707e919ca77581b59eb673c126261ef04546
brynjarmorka <morkabrynjar+github@gmail.com>
2023-03-13 10:56:04 +0100

    progging

--- helper_functions.py
+++ helper_functions.py
@@ -1,9 +1,10 @@
 # this is a file for helper functions
 import hyperspy.api as hs
 import numpy as np
+import pandas as pd
 
 
-def elementlines(element, all=True):
+def elementlines(element, all=True, give_return=False):
     """
     Given an element, return the x-ray lines using the HyperSpy library.
 
@@ -14,7 +15,6 @@ def elementlines(element, all=True):
     all : bool, optional
         Give all lines or jst La, Lb, Ka, and Kb, by default True
     """
-    print(element)
     d = hs.material.elements[element].Atomic_properties.Xray_lines.as_dictionary()
     lines = []
     for key in d.keys():
@@ -23,6 +23,9 @@ def elementlines(element, all=True):
         if key == "La" or key == "Lb" or key == "Ka" or key == "Kb":
             lines.append([d[key]["energy (keV)"], key])
     lines.sort(key=lambda x: x[0])
+    if give_return:
+        return lines
+    print(element)
     for line in lines:
         print(f"{line[1]:5} {line[0]:.4f}")
 
@@ -38,9 +41,32 @@ def nearestlines(energy):
 
     Returns
     -------
-    list
-        list of closest x-ray lines, closest first
+    pd.DataFrame
+        df of closest x-ray lines, closest first
     """
-    return hs.eds.get_xray_lines_near_energy(energy)
+    lines = hs.eds.get_xray_lines_near_energy(energy)
+    energies = [theoretical_energy(line) for line in lines]
+    df = pd.DataFrame({'line': lines, 'energy [keV]': energies})
+    df.set_index('line', inplace=True)
+    df['delta [eV]'] = (df['energy [keV]'] - energy) * 1000
+    return df
 
 
+
+def theoretical_energy(line):
+    """
+    Return the theoretical energy of a given x-ray line.
+
+    Parameters
+    ----------
+    line : str
+        X-ray line, e.g. 'Ga_Ka'
+
+    Returns
+    -------
+    float
+        Energy of the x-ray line in keV
+    """
+    element = line.split('_')[0]
+    line_name = line.split('_')[1]
+    return hs.material.elements[element]['Atomic_properties']['Xray_lines'][line_name]['energy (keV)']
